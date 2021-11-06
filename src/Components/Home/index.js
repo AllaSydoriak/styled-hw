17dc3ca6a3877a4a124e99dec648bd8c40904fa5
AllaSydoriak <alochka12@ukr.net>
2021-11-06 23:14:27 +0200

    add styled props

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,7 +8,18 @@ import styled from '../../helpers/styled';
 const Div = styled.div`
   background-color: red;
   color: ${(props) => props.color};
-  text-decoration: ${(props) => props.isLink ? 'underlined' : 'none'};
+  text-decoration: ${(props) => props.isLink ? 'underline' : 'none'};
+`;
+
+const PlainButton = styled.button`
+  cursor: pointer;
+  font-size: 18px;
+  margin-top: 10px;
+  border-radius: 6px;
+  padding: 10px 20px;
+  background-color: transparent;
+  color: ${(props) => props.type === 'plain' ? 'white' : 'yellow' };
+  border: 1px solid ${(props) => props.type === 'plain' ? 'white' : 'yellow' };;
 `;
 
 const App = () => {
@@ -16,7 +27,8 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Div isLink={false} color="white">my div</Div>
+        <Div color="white">my div</Div>
+        <PlainButton type="color">My button</PlainButton>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/helpers/styled.js
+++ src/helpers/styled.js
@@ -1,23 +1,41 @@
 import React from 'react';
 
-const randomClassname = () => Math.random().toString(36).substr(2, 5);
+const getRandomClassname = () => Math.random().toString(36).substr(2, 5);
 
-const createStyles = (styles) => {
-    const className = randomClassname();
+const setStyles = (styles) => {
+    const className = getRandomClassname();
     const style = document.createElement('style');
 
     style.innerHTML = `.${className} { ${styles} }`;
     document.getElementsByTagName('head')[0].appendChild(style);
 
     return className;
-}
+};
+
+const createStyles = (strings, callbacks, values) => strings.reduce((acc, value, index) => {
+    if(callbacks.length > index) {
+        const style = callbacks[index](values);
+        return `${acc}${value}${style}`;
+    }
+    return `${acc}${value}`;
+}, '');
+
 
 const styled = {
     div: (strings, ...stylesProps) => {    
         return ({ children, ...componentProps }) => {
-            const styles = createStyles(strings, stylesProps.map((func) => func(componentProps)));
+            const resultString = createStyles(strings, stylesProps, componentProps);
+            const styles = setStyles(resultString);
 
             return <div className={styles}>{children}</div>
+        }
+    },
+    button: (strings, ...stylesProps) => {    
+        return ({ children, ...componentProps }) => {
+            const resultString = createStyles(strings, stylesProps, componentProps);
+            const styles = setStyles(resultString);
+
+            return <button className={styles}>{children}</button>
         }
     },
 };
